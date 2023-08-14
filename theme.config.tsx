00e7a3796e5f74446d03be1b8c820a46f6be9f82
navaneeth technical <navaneethreddyboda@gmail.com>
2023-08-14 21:34:41 +0530

    edited default docs and added some functions

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>rn-media-library</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/navvitech/rn-media-library',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/navvitech/rn-media-library',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'rn-media-library Documentation',
   },
 }
 
