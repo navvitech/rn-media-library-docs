ee0eb9cf05be13c8c3f8846b99defc5c89ba7dcb
navaneeth technical <navaneethreddyboda@gmail.com>
2023-08-27 18:30:06 +0530

    seprated all the functions into individual pages

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
+import './styles.css';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
+}
